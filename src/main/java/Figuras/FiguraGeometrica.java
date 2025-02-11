127b818ed4027d21d146e9e49d9808625aaf5f28
d4lion <d4lion.dev@gmail.com>
2025-02-11 07:50:58 -0500

    Ejercicio 8.3 resuelto

---
+++ src/main/java/Figuras/FiguraGeometrica.java
@@ -0,0 +1,22 @@
+package Figuras;
+
+public class FiguraGeometrica {
+    private double volumen;
+    private double superficie;
+
+    public double getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+}
---
+++ src/main/java/Figuras/Piramide.java
@@ -0,0 +1,49 @@
+package Figuras;
+
+public class Piramide extends FiguraGeometrica {
+    private double base;
+    private double altura;
+    private double apotema;
+
+    public Piramide(double base, double altura, double apotema){
+        this.base = base;
+        this.altura = altura;
+        this.apotema = apotema;
+        this.setVolumen(calcularVolumen());
+        this.setSuperficie(calcularSuperficie());
+    }
+
+    public double calcularVolumen() {
+        return (Math.pow(base, 2.0) * altura) / 3.0;
+    }
+
+    public double calcularSuperficie() {
+        double areaBase = Math.pow(base, 2.0);
+        double areaLado = 2.0 * base * apotema;
+        return areaBase + areaLado;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getApotema() {
+        return apotema;
+    }
+
+    public void setApotema(double apotema) {
+        this.apotema = apotema;
+    }
+}
